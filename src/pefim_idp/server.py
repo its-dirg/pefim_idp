9fb9b01408e304c3ce200a3fed35b5afc01013c4
Hans Hörberg <hans.horberg@umu.se>
2015-05-25 17:42:07 +0200

    Fix for pysaml2 new encryption framework.

--- src/pefim_idp/server.py
+++ src/pefim_idp/server.py
@@ -336,9 +336,7 @@ class SSO(Service):
 
                 _resp = IDP.create_authn_response(
                     identity, userid=self.user,
-                    encrypt_cert=encrypt_cert,
-                    encrypt_assertion_self_contained=True,
-                    encrypted_advice_attributes=True,
+                    encrypt_cert_advice=encrypt_cert,
                     **resp_args)
             except Exception as excp:
                 logging.error(exception_trace(excp))
